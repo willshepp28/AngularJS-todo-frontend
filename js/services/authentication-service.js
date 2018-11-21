1590efb4a247711cba10b2e3584bf2591bd0d5ac
willshepp28 <willsheppard28@outlook.com>
2018-11-20 22:12:59 -0500

    included register method in authentication service

--- js/services/authentication-service.js
+++ js/services/authentication-service.js
@@ -3,10 +3,35 @@ angular.module("todoApp")
 
 
 
-function AuthenticationService(){
+function AuthenticationService($http){
 
     var self = this;
 
+
+    self.register = function(username, password){
+
+        $scope.data = {
+            username: username,
+            password: password
+        }
+
+        $http({
+            method: "POST",
+            url: "http://localhost:3000/api/v1/auth/signup",
+            data: $scope.data
+        })
+        .then(function success() {
+            console.log("You successfully signed up")
+        }, function error(){
+            console.log("error");
+        })
+    }
+
+
+
+    self.login = function(username, password) {
+        
+    }
 
     // Checks if their is a token in localstorage
     self.getToken = function(){
